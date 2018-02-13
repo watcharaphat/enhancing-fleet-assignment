0b71cac6190fba3cb5ffd059242a7c3b5df47a6e
watcharaphat <watcharaphat.3166@gmail.com>
2018-02-13 13:19:09 +0700

    calculate lowest aircraft use

--- build/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var test = exports.test = 5;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy90ZXN0LmpzIl0sIm5hbWVzIjpbInRlc3QiXSwibWFwcGluZ3MiOiI7Ozs7O0FBQU8sSUFBTUEsc0JBQU8sQ0FBYiIsImZpbGUiOiJ0ZXN0LmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNvbnN0IHRlc3QgPSA1O1xuIl19
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import Moment from 'moment';
+import { extendMoment } from 'moment-range';
+
+const moment = extendMoment(Moment);
+
 const util = require('util');
 const fs = require('fs');
 const Papa = require('papaparse');
@@ -32,6 +37,9 @@ function convertToMin(timeString) {
 
 function convertToTimeString(minTime) {
   let hour = Math.floor(minTime/60);
+  if (hour < 10) {
+    hour = '0'.concat(hour.toString());
+  }
 
   let minute = minTime%60;
 
@@ -47,7 +55,136 @@ function convertTime(timeString) {
   return convertToTimeString(convertToMin(timeString));
 }
 
+function convertTimePlusTurnTime(timeString, turnTime) {
+  return convertToTimeString(convertToMin(timeString) + turnTime);
+}
+
+function countOverlaps(flightTable, aircraft) {
+  let maxOverlaps = 0;
+
+  for (let i = 0; i < flightTable.length; i++) {
+    let overlapsCount = 0;
+    for (let j = 0; j < flightTable.length; j++) {
+      if (i === j) continue;
+
+      if (flightTable[i].equipmentName === aircraft && flightTable[j].equipmentName === aircraft) {
+        const range1 = flightTable[i].momentRange;
+        const range2 = flightTable[j].momentRange;
+
+        if (range1.overlaps(range2)) {
+          overlapsCount += 1;
+          maxOverlaps = Math.max(maxOverlaps, overlapsCount);
+        }
+      }
+    }
+  }
+
+  return maxOverlaps;
+}
+
+async function dynamicAssign(aircraftList, flightTable) {
+  console.log('Fleet Assigning.');
+
+  // sort by arrTime
+  flightTable.sort(compareFlight);
+
+  let aList = [];
+  let bList = [];
+  let cList = [];
+
+  flightTable.forEach((row) => {
+    switch(row.equipmentName) {
+      case '(PG) 319':
+        aList.push(row);
+        break;
+      case '(PG) 320':
+        bList.push(row);
+        break;
+      case '(PG) AT7':
+        cList.push(row);
+        break;
+    }
+  });
+
+  // await optimum(aList);
+
+  // aList.forEach((row) => {
+  //   if (row.aircraftNo) {
+  //     console.log(row.flight);
+  //   }
+  // });
+
+  const a = countOverlaps(flightTable, aircraftList[0]);
+  const b = countOverlaps(flightTable, aircraftList[1]);
+  const c = countOverlaps(flightTable, aircraftList[2]);
+
+  console.log(`a: ${a}`);
+  console.log(`b: ${b}`);
+  console.log(`c: ${c}`);
+}
+
+let currentAircraft = 1;
+let latestRow = null;
+
+async function optimum(schedule) {
+  while (!isDone(schedule)) {
+    for (let i = 0; i < schedule.length; i++) {
+      const row = schedule[i];
+  
+      if (!isConflict(row, latestRow)) {
+        row.aircraftNo = currentAircraft;
+        latestRow = row;
+        continue;
+      }
+    }
+  
+    latestRow = null;
+    currentAircraft += 1;
+  }
+}
+
+function isConflict(row1, row2) {
+  if (!row2) {
+    console.log('not conflict');
+    return false;
+  }
+
+  console.log(`isConflict: ${row1.momentRange.overlaps(row2.momentRange)}`);
+
+  return row1.momentRange.overlaps(row2.momentRange);
+}
+
+function isDone(schedule) {
+  for (let i = 0; i < schedule.length; i++) {
+    const row = schedule[i];
+
+    if (!row.aircraftNo) {
+      console.log('not done')
+      return false;
+    }
+  }
+
+  console.log('done');
+
+  // all row in schedule assigned
+  return true;
+}
+
+function compareFlight(row1, row2) {
+  if (row1.momentRange.end < row2.momentRange.end) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
 async function main() {
+  const turnTime = 30;
+
+  const aircraftList = ['(PG) 319', '(PG) 320', '(PG) AT7'];
+
+  // ****************************** 
+
   const dataText = await getData();
   const data = Papa.parse(dataText, {
     header: true,
@@ -59,16 +196,24 @@ async function main() {
       row[key] = row[key].replace(/\s\s+/g, ' ');
     }
 
+    let tmp = row.arrTime;
+
     row.depTime = convertTime(row.depTime);
     row.arrTime = convertTime(row.arrTime);
 
-    // console.log(row);
+    tmp = convertTimePlusTurnTime(tmp, turnTime);
+
+    const date = moment().format('YYYY-MM-DD');
+    const depMoment = moment(`${date} ${row.depTime}`);
+    const arrMoment = moment(`${date} ${tmp}`);
+
+    const range = moment.range(depMoment, arrMoment);
+    row['momentRange'] = range;
+
+    row.revenue = Number(row.revenue);
   });
 
-  console.log(data);
-
-  // const t = convertToTimeString(convertToMin('600'));
-  // console.log(t);
+  await dynamicAssign(aircraftList, data);
 }
 
 main();
