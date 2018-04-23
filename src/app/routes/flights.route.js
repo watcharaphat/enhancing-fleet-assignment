2c1768c9fcbbe0cec465ffe6eb860cae8704ade6
watcharaphat <watcharaphat.3166@gmail.com>
2018-04-24 00:19:32 +0700

    really fix cores

--- src/app/routes/flights.route.js
+++ src/app/routes/flights.route.js
@@ -1,9 +1,8 @@
-import cors from 'cors';
 import * as flight from '../controllers/flights.controller';
 import parseSchedule from '../../modules/utils/ParseSchedule';
 import fleetAssign from '../../modules/fleet-assignment/FleetAssign';
 
 export default function(app) {
-  app.route('/flights', cors())
+  app.route('/flights')
     .get(flight.list, parseSchedule, fleetAssign);
 }
--- src/config/express.js
+++ src/config/express.js
@@ -3,6 +3,14 @@ import flightsRoute from '../app/routes/flights.route';
 
 export default function() {
   const app = express();
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+    next();
+  });
+
   flightsRoute(app);
 
   return app;
